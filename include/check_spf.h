cc07ca51d2c377dce08203ff9e2b84040f5dd16c
eino.tuominen <eino.tuominen@529a611b-dd24-0410-8390-ad8781149581>
2007-02-10 07:11:20 +0000

    A skeleton for spf checks.

--- include/check_spf.h
+++ include/check_spf.h
@@ -1,6 +1,5 @@
 /*
- * Copyright (c) 2006 Eino Tuominen <eino@utu.fi>
- *                    Antti Siira <antti@utu.fi>
+ * Copyright (c) 2007 Eino Tuominen <eino@utu.fi>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -15,8 +14,9 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-/* 
- * Interface requires this file
- */
+#ifndef CHECK_SPF_H
+#define CHECK_SPF_H
 
-typedef int foo;
+void spf_init();
+
+#endif /* CHECK_SPF_H */
---
+++ src/check_spf.c
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2007
+ *                    Eino Tuominen <eino@utu.fi>
+ *                    Antti Siira <antti@utu.fi>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "common.h"
+#include "check_spf.h"
+#include "srvutils.h"
+#include "utils.h"
+#include "worker.h"
+
+/* the cleanup routine */
+int
+cleanup_spfc(void *state)
+{
+	SPF_server_t *spf_server;
+
+	spf_server = (SPF_server_t *)state;
+        if (spf_server)
+                SPF_server_free(spf_server);
+	return 0;
+}
+
+int
+spfc(thread_ctx_t *thread_ctx, edict_t *edict)
+{
+	struct timespec ts, start, now, timeleft;
+	chkresult_t *result;
+	grey_tuple_t *request;
+        SPF_server_t *spf_server = NULL;
+        SPF_request_t *spf_request = NULL;
+        SPF_response_t *spf_response = NULL;
+        SPF_response_t *spf_response_2mx = NULL;
+
+	logstr(GLOG_DEBUG, "spfc called");
+
+	request = (grey_tuple_t *)edict->job;
+	assert(request);
+
+	result = (chkresult_t *)Malloc(sizeof(chkresult_t));
+	memset(result, 0, sizeof(*result));
+        result->judgment = J_UNDEFINED;
+
+	/* initialize if we are not yet initialized */
+	if (NULL == thread_ctx->state) {
+		/* Initialize */
+		spf_server = SPF_server_new();
+		if (NULL = spf_server) {
+			logstr(GLOG_ERROR, "SPF_server_new failed");
+			goto FINISH;
+		}
+		thread_ctx->state = spf_server;
+		thread_ctx->cleanup = &cleanup_spfc;
+	} else {
+		spf_server = (SPF_server_t *)thread_ctx->state;
+	}
+
+	/* Now we are ready to query */
+	spf_request = SPF_request_new(spf_server);
+	
+	ret = SPF_request_set_ipv4_str(spf_request, request->client_ip);
+	if (ret) {
+		logstr(GLOG_ERROR, "invalid IP address %s", request->client_ip);
+		goto ERROR;
+	}
+
+
+ERROR:
+        if (spf_request)
+                SPF_request_free(spf_request);
+FINISH:
+
+	send_result(edict, result);
+	
+	logstr(GLOG_DEBUG, "spfc returning");
+	request_unlink(request);
+
+	return 0;
+}
+
+void
+spf_init()
+{
+	thread_pool_t *pool;
+
+	/* initialize the thread pool */
+        logstr(GLOG_INFO, "initializing spf checker thread pool");
+	pool = create_thread_pool("spf", &spfc);
+        if (pool == NULL)
+                daemon_perror("create_thread_pool");
+
+	/* This is a definitive check */
+	register_check(pool, true);
+}
