68d7588405b8740a00f195f65a2af66f83120fbb
antti.siira <antti.siira@529a611b-dd24-0410-8390-ad8781149581>
2008-04-18 05:45:18 +0000

    Removed unnecessary log messages.

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -28,8 +28,6 @@
 #include "utils.h"
 #include "thread_pool.h"
 
-#include "signal.h"
-
 /* internals */
 static void *thread_pool(void *arg);
 
@@ -85,7 +83,6 @@ thread_pool(void *arg)
 
 		clock_gettime(CLOCK_TYPE, &now);
 		waited = ms_diff(&now, &pool_ctx->last_idle_check);
-		logstr(GLOG_DEBUG, "now %d", now.tv_sec);
 
 		if (waited > IDLETIME) {
 			/* update the reference time */
@@ -95,12 +92,11 @@ thread_pool(void *arg)
 				/* check the watchdog status */
 				dogp = pool_ctx->wdlist;
 				while (dogp) {
-				  logstr(GLOG_DEBUG, "WD last seen %d", dogp->last_seen.tv_sec); 
 					lastseenms = ms_diff(&now, &dogp->last_seen);
 					if (lastseenms > pool_ctx->watchdog_time) {
 						/* a stuck thread */
-						logstr(GLOG_WARNING, "thread #%x of pool '%s' stuck, last seen %d ms ago (%d).",
-						       (uint32_t)dogp->tid, pool_ctx->info->name, lastseenms, dogp->last_seen.tv_sec );
+						logstr(GLOG_WARNING, "thread #%x of pool '%s' stuck, last seen %d ms ago.",
+						       (uint32_t)dogp->tid, pool_ctx->info->name, lastseenms);
 						pthread_kill(dogp->tid, SIGALRM);
 					}
 					dogp = dogp->next;
