3421cb081dc7deebeb76b870f6a1acbbffc7bc67
antti.siira <antti.siira@529a611b-dd24-0410-8390-ad8781149581>
2008-05-22 09:53:02 +0000

    New, more rigorous tests

--- src/sha256-test.c
+++ src/sha256-test.c
@@ -21,18 +21,29 @@
 #include <stdio.h>
 #include <string.h>
 #include "sha256.h"
+#include "srvutils.h"
+#include "sha256-test.h"
 
 #define MAX_MESSAGE_LEN 1024
 
-typedef struct {
-  char *message;
-  char *reference_digest;
-} test_vector;
-
-test_vector test_vectors[] = { {"The quick brown fox jumps over the lazy dog","d7a8fbb3 07d78094 69ca9abc b0082e4f 8d5651e4 6d3cdb76 2d02d0bf 37c9e592"},
-			       {"The quick brown fox jumps over the lazy cog","e4c4d8f3 bf76b692 de791a17 3e053211 50f7a345 b46484fe 427f6acc 7ecc81be"},
-			       {"","e3b0c442 98fc1c14 9afbf4c8 996fb924 27ae41e4 649b934c a495991b 7852b855"},
-			       {NULL,NULL} };
+void 
+verbose_result(bool ok, char *message, char *digest_hex, char *reference_digest)
+{
+	char tmp[40] = {0x00};
+	strncpy(tmp, message, 36);
+	
+	if (strnlen(message) > 36) {
+		strncat(tmp+36, "...", 3);
+	}
+		
+	if (ok) {
+		printf("\nMessage: '%s'\n\t          digest: '%s'\n\treference digest: '%s'.",
+		       tmp, digest_hex, reference_digest);
+	} else {
+		printf("\nERROR: For string %s digest '%s' and reference digest '%s' differ.\n",
+		       tmp, digest_hex, reference_digest);
+	}
+}
 
 int
 main(int argc, char **argv)
@@ -43,38 +54,54 @@ main(int argc, char **argv)
 	char digest_hex[MAX_MESSAGE_LEN] = { 0x00 };
 	int error_count = 0;
 	test_vector *test;
+	char *long_message;
 
 	printf("Check: sha256\n");
-	
+
+	/* Known test vectors */
 	for (test=test_vectors ; test->message && test->reference_digest ; test++) {
 		strncpy(message, test->message, MAX_MESSAGE_LEN);
 		strncpy(reference_digest, test->reference_digest, MAX_MESSAGE_LEN);
-
-		printf("  Checking '%s'...  ", message);
-		fflush(stdout);
 
 		digest = sha256((sha_byte_t *)message, strlen(message));
 		snprintf(digest_hex, MAX_MESSAGE_LEN, "%08x %08x %08x %08x %08x %08x %08x %08x", digest.h0,
 		    digest.h1, digest.h2, digest.h3, digest.h4, digest.h5, digest.h6, digest.h7);
 
 		if (strncmp(digest_hex, reference_digest, MAX_MESSAGE_LEN) != 0) {
-			if (argc>2) {
-				printf("\nERROR: For string %s digest '%s' and reference digest '%s' differ.\n",
-				       message, digest_hex, reference_digest);
-			} else {
-				printf("Fail.\n");
+			if (argc>1) {
+				verbose_result(FALSE, message, digest_hex, reference_digest);
 			}
 
 			error_count++;
 		} else {
-			if (argc>2) { 
-				printf("\nMessage: '%s'\n\t          digest: '%s'\n\treference digest: '%s'.",
-				       message, digest_hex, reference_digest);
-			} else {
-				printf("Done.\n");
+			if (argc>1) { 
+				verbose_result(TRUE, message, digest_hex, reference_digest);
 			}
 		}
 	}
+
+	/* Known long message */
+	long_message = Malloc(sizeof(char)*1000001);
+	memset(long_message, 'a', 1000000);
+	long_message[1000000] = 0x00;
+	strncpy(reference_digest, "cdc76e5c 9914fb92 81a1c7e2 84d73e67 f1809a48 a497200e 046d39cc c7112cd0",MAX_MESSAGE_LEN);
+
+	digest = sha256((sha_byte_t *)long_message, strlen(long_message));
+	snprintf(digest_hex, MAX_MESSAGE_LEN, "%08x %08x %08x %08x %08x %08x %08x %08x", digest.h0,
+		 digest.h1, digest.h2, digest.h3, digest.h4, digest.h5, digest.h6, digest.h7);
+	
+	if (strncmp(digest_hex, reference_digest, MAX_MESSAGE_LEN) != 0) {
+		if (argc>1) {
+			verbose_result(FALSE, long_message, digest_hex, reference_digest);
+		}
+		
+		error_count++;
+	} else {
+		if (argc>1) {
+			verbose_result(TRUE, long_message, digest_hex, reference_digest);
+		}
+	}
+	Free(long_message);
 
 	return error_count > 0;
 }
